75142ebd20a493b95e9e629946aac9494748710a
engibaryan <xachik.engibar@gmail.com>
2022-03-22 18:26:43 +0400

    appling changes

--- index.js
+++ index.js
@@ -1,62 +1,49 @@
-const express = require('express')
-const req = require('express/lib/request')
-const app = express()
-const port = 5006
- 
-app.use(express.json())
- 
- 
+const express = require("express");
+const app = express();
+const PORT = 5006;
+
+app.use(express.json());
+
 let data = [
-     {
-  "Id": 1 ,
-  "Title": "scanning",
-  "isDone": true
- },
- 
- 
- {
-  "Id": 2 ,
-  "Title": "reading",
-  "isDone": false
- },
- 
- 
- {
-  "Id": 3 ,
-  "Title": "running ",
-  "isDone": true
- },
- 
- 
- {
-  "Id": 4 ,
-  "Title": "playing",
-  "isDone": true
- }
- 
-]
- 
- 
+  {
+    Id: 1,
+    Title: "scanning",
+    isDone: true,
+  },
+
+  {
+    Id: 2,
+    Title: "reading",
+    isDone: false,
+  },
+
+  {
+    Id: 3,
+    Title: "running ",
+    isDone: true,
+  },
+
+  {
+    Id: 4,
+    Title: "playing",
+    isDone: true,
+  },
+];
+
 app.get("/notes/:id", (req, res) => {
-  return res.status(200).send(data[req.params.id-1])
-  
+  return res.status(200).send(data[req.params.id - 1]);
 });
- 
+
 app.post("/notes/:id", (req, res) => {
-   data[req.params.id-1]=req.body;
-   return res.send(data);
-});  
- 
-app.delete("/note/:id", (req,res) => {
-   data[req.params.id-1] = undefined;
-   return res.send(data);
-     
+  data[req.params.id - 1] = req.body;
+  return res.send(data);
 });
- 
- 
- 
- 
-app.listen(port, () => {
-   console.log(`app listening at http://localhost:${port}`)
- });
- 
+
+app.delete("/note/:id", (req, res) => {
+  data[req.params.id - 1] = undefined;
+  return res.send(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`app listening at http://localhost:${PORT}`);
+});
